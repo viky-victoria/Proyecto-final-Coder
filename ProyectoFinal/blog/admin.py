243fc9dae314292467b7cc2a2bfacf2147074021
Victoria <mvciordia@gmail.com>
2022-10-09 16:37:41 -0300

    modelos

--- ProyectoFinal/blog/admin.py
+++ ProyectoFinal/blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from blog.models import Autor, Articulo
+from blog.models import Autor, Articulo, Seccion
 
 # Register your models here.
 
-admin.site.register 
+admin.site.register(Articulo)
+admin.site.register(Autor) 
+admin.site.register(Seccion)
--- ProyectoFinal/blog/models.py
+++ ProyectoFinal/blog/models.py
@@ -1,20 +1,33 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
 
 class  Autor(models.Model):
+    class Meta:
+        verbose_name_plural= "Autores"
+    
     
     nombre=models.CharField(max_length=30)
     apellido=models.CharField(max_length=30)
     profesion=models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.nombre   
+        
+         
 
 class  Articulo(models.Model):
-    
+    class Meta:
+        verbose_name_plural= "Articulos"
+      
     titulo=models.CharField(max_length=30)
     texto=models.CharField(max_length=1000)
     fecha=models.DateField(null=True)
 
 class Seccion(models.Model):
-   
-   nombre=models.CharField(max_length=30)
+    class Meta:
+        verbose_name_plural= "Secciones"
+       
+    nombre=models.CharField(max_length=30)
    
